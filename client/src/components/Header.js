7e41d31fa77b32c5f702d62bf5129a866063ba0a
Jaish Singh <jaishsingh@hotmail.com>
2022-08-10 19:40:57 +0100

    .

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from '../media/placeholder-logo-5-33978452.png'
+import '../css/header.css'
+
+function Header() {
+	return (
+	<div className = 'Header'>
+		<img src={logo} className='logo'/>
+		<button>  </button>
+	</div>);
+}
+
+export default Header;
